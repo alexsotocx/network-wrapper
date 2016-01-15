0320ba265a71a577bcce6a00f46a4c6a03419e63
Alexander Soto Cardona <asoto@innventto.com>
2016-01-14 23:02:25 -0500

    Ready ping

--- src/pingParser.js
+++ src/pingParser.js
@@ -1,16 +1,38 @@
 var os = require('os');
 
 var PingParser = {
-	Windows: function() {},
+	Windows: function(output, resolve, reject) {
+		var ms = output.match(/\s*(tiempo|time|tempo|temps)\s*=\s*(.+)\s*ms/)
+		var ip = output.match(/\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b/)
+		if(ms != null) {
+			ms = ms[2].trim();
+			ip = ip[0].trim();
+			return resolve([ip, ms])
+		} else {
+			return reject(new Error("Can't parse the output"));
+		}
+	},
 	Linux: function(output, resolve, reject) {
-		console.log("Calleddddd");
-		output = output.split('\r\n');
-		resolve(output);
+		var ms = output.match(/\s*(tiempo|time|tempo|temps)\s*=\s*(.+)\s*ms/)
+		var ip = output.match(/\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b/)
+		if(ms != null) {
+			ms = ms[2].trim();
+			ip = ip[0].trim();
+			return resolve([ip, ms])
+		} else {
+			return reject(new Error("Can't parse the output"));
+		}
 	},
 	Darwin: function(output, resolve, reject) {
-		console.log("Calleddddd");
-		output = output.split('\n');
-		resolve(output);
+		var ms = output.match(/\s*(tiempo|time|tempo|temps)\s*=\s*(.+)\s*ms/)
+		var ip = output.match(/\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b/)
+		if(ms != null) {
+			ms = ms[2].trim();
+			ip = ip[0].trim();
+			return resolve([ip, ms])
+		} else {
+			return reject(new Error("Can't parse the output"));
+		}
 	},
 	parse: function(output) {
 		var parser = this[os.type()];
--- src/pingWrapper.js
+++ src/pingWrapper.js
@@ -4,13 +4,13 @@ var os = require('os');
 var Ping = {
 	command: function(options) {
 		var osFunctions = {
-			Windows: "ping -n :count -i :ttl -w :timeout :domainName",
-			Linux: "ping -c :count -m :ttl -t :timeout :domainName",
-			Darwin: "ping -c :count -m :ttl -t :timeout :domainName"
+			Windows: "ping -n 1 -i :ttl -w :timeout :domainName",
+			Linux: "ping -c 1 -m :ttl -t :timeout :domainName",
+			Darwin: "ping -c 1 -m :ttl -t :timeout :domainName"
 		};
 		var osType = os.type();
-		console.log(osFunctions);
 		var osCommand = osFunctions[osType];
+		var isWindows = (osType == "Windows");
 		var simbols = osCommand.match(/\:([^ ]+)/g)
 		simbols.forEach(function(simbol) {
 			var dataSimbol = simbol.substring(1); //Removes the ':' of the simbol
@@ -24,17 +24,17 @@ var Ping = {
 		options = {
 			ttl: (options.ttl || 20),
 			timeout: (options.timeout || 2000),
-			count: (options.count || 10),
 			domainName: domainName
 		}
 		var command = this.command(options);
 		return new Promise(function(resolve, reject) {
 			exec(command, function(err, stdout, stderr) {
+				setTimeout(function() {
+					return reject(new Error("Operation Timeout"));
+				}, options.timeout * 20);
 				if(err) {
-					//console.log(err.toString());
 					return reject(err);
 				} else {
-					//console.log(stdout);
 					return resolve(stdout);
 				}
 			}.bind(this));
